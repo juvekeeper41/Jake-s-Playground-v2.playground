3bbf9b0386b7d3cd95ec6d640348758aca193536
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-15 08:35:56 -0600

    Created assignment and tried out functions.

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let jake = "👌🏾👶🏿"
+jake
+jake
+let jakeIsAwesome = true
+let jakeIsFunny = false
+jakeIsAwesome
+jakeIsFunny
+if jakeIsAwesome {
+    print("Jake is awesome!")
+} else {
+    print("Jake is not awesome!")
+}
+jakeIsAwesome
+
